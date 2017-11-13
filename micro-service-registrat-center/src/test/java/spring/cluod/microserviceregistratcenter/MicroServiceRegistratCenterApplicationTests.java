7be3fe80e3d50cfd3917ce163535d60b0b24fece
XXP0068\Administrator <xuxiping@xinyunlian.com>
2017-11-13 20:08:12 +0800

    初始化工程

--- micro-service-registrat-center/src/test/java/spring/cluod/microserviceregistratcenter/MicroServiceRegistratCenterApplicationTests.java
+++ micro-service-registrat-center/src/test/java/spring/cluod/microserviceregistratcenter/MicroServiceRegistratCenterApplicationTests.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MicroServiceRegistratCenterApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
